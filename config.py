1014fab0ea12c7616c7f3e118a88ba9fcf5db32d
miuguelt <miuguelt@gmail.com>
2024-11-25 15:33:49 -0500

    bd

--- config.py
+++ config.py
@@ -13,10 +13,10 @@
 import os
 
 class Config:
-    USER = os.getenv('MYSQL_USER', 'mysqlu')
-    PASSWORD = os.getenv('MYSQL_PASSWORD', '1234567abc')
+    USER = os.getenv('MYSQL_USER', 'fincau')
+    PASSWORD = os.getenv('MYSQL_PASSWORD', 'fincac')
     HOST = os.getenv('MYSQL_HOST', '85.239.241.150')  
-    PORT = os.getenv('MYSQL_PORT', 3316)
+    PORT = os.getenv('MYSQL_PORT', 3306)
     DATABASE = os.getenv('MYSQL_DATABASE', 'finca')
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
