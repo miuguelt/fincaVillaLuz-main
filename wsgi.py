3927e6758e53fdabadd7de0e5695840c69232277
miuguelt <miuguelt@gmail.com>
2025-01-03 18:34:40 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,6 @@ from app import create_app
 # Crear la aplicación Flask
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
     # Permitir que Gunicorn ejecute la aplicación
     app.run()
