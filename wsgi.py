8daddc1a31ecacc5879840aad974767df0637f04
miuguelt <miuguelt@gmail.com>
2025-01-07 00:24:39 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,11 @@ from flask_cors import CORS
 # Crear la aplicación Flask
 app = create_app()
 
-CORS(app, supports_credentials=True, origins=["https://mifinca.isladigital.xyz"], methods=["GET", "POST", "PUT", "DELETE"], headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"], expose_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"], allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"], max_age=3600)
+#CORS(app, supports_credentials=True, origins=["https://mifinca.isladigital.xyz"], methods=["GET", "POST", "PUT", "DELETE"], headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"], expose_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"], allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"], max_age=3600)
+cors_config_str = os.environ.get('CORS_CONFIG')
+cors_config = json.loads(cors_config_str)  # Convert string to dictionary
 
+CORS(app, **cors_config)
 
 if __name__ == "__main__":
     # Permitir que Gunicorn ejecute la aplicación
