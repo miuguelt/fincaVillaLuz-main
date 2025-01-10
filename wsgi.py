22659668a4b816802ee79abf11ae5b73aec878c3
miuguelt <miuguelt@gmail.com>
2025-01-10 15:24:43 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,6 @@
 from app import create_app
-from flask_cors import CORS
 
 app = create_app()
-
-CORS(app, resources={r"/*": {"origins": "*"}}, 
-supports_credentials=True, 
-methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],    
-allow_headers=["Content-Type", "Authorization"])
 
 if __name__ == "__main__":
     app.run()
