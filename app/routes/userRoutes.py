9d3e6d37624fd7dd8c847248b49653999639a03c
miuguelt <miuguelt@gmail.com>
2025-01-11 14:52:36 -0500

    Update userRoutes.py

--- app/routes/userRoutes.py
+++ app/routes/userRoutes.py
@@ -34,9 +34,18 @@ def get_user_roles():
     roles_data = [{"role": role.value , "count": count} for role, count in roles]
     return jsonify(roles_data)
 
-@bp.route('/', methods=['POST'], strict_slashes=False)
+
+@bp.route('/', methods=['POST'])
 def create_user():
     data = request.get_json()
+    
+    # Verifica que todos los campos requeridos estén presentes
+    required_fields = ["fullname", "email", "password", "phone", "address", "identification", "role", "status"]
+    for field in required_fields:
+        if field not in data:
+            return jsonify({"error": f"Campo requerido faltante: {field}"}), 400
+    
+    # Crea el usuario
     user = User(**data)
     try:
         db.session.add(user)
@@ -44,7 +53,17 @@ def create_user():
         return jsonify(user.to_json()), 201
     except IntegrityError as e:
         db.session.rollback()
-        return jsonify({"error": str(e.orig)}), 400
+        # Devuelve un mensaje de error más específico
+        error_info = str(e.orig)
+        if "duplicate key" in error_info.lower():
+            return jsonify({"error": "El correo electrónico ya está en uso"}), 400
+        elif "null value" in error_info.lower():
+            return jsonify({"error": "Faltan campos requeridos"}), 400
+        else:
+            return jsonify({"error": error_info}), 400
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
 
 @bp.route('/<int:id>', methods=['PUT'], strict_slashes=False)
 def update_user(id):
