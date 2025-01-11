dbfb3f2da4a2a0bbf5b0400b5679b189c303041f
miuguelt <miuguelt@gmail.com>
2025-01-11 12:33:35 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    cert_path = 'proxy/cert.pem'
-    key_path = 'proxy/key.pem'
-    app.run(ssl_context=(cert_path, key_path))
+    app.run()
