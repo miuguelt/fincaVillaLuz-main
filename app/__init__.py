6378d0171685c0006f0c5bbf74fdb27f809d72e2
miuguelt <miuguelt@gmail.com>
2025-04-11 15:17:37 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,19 +7,12 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     app.config.from_object('config.Config')
     app.config['JWT_SECRET_KEY'] = 'super-secret'
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
     jwt = JWTManager(app)
- 
-    # Configurar CORS para permitir solicitudes desde cualquier origen
-    CORS(app, resources={r"/*": {"origins": "https://mifinca.isladigital.xyz/*", 
-    "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-    "allow_headers": ["Content-Type", "Authorization"],
-    "supports_credentials": True
-    }
-    })
+
     
     db.init_app(app)
 
