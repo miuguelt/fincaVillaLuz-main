db28e78619e9c5839733210e887a93eac98e8ada
miuguelt <miuguelt@gmail.com>
2025-05-25 11:15:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     PASSWORD = os.getenv('DB_PASSWORD')
     HOST = os.getenv('DB_HOST')  
     PORT = os.getenv('DB_PORT')
-    DATABASE = os.getenv('MYSQL_DATABASE', 'finca')
+    DATABASE = os.getenv('DB_USER')
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
