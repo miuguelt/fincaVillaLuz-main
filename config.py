621e0e50374da5ffc79c041999d7030226402526
miuguelt <miuguelt@gmail.com>
2025-05-25 11:11:14 -0500

    venv

--- config.py
+++ config.py
@@ -1,22 +1,10 @@
-# import os
-
-# class Config:
-#     USER = os.getenv('MYSQL_USER', 'root')
-#     PASSWORD = os.getenv('MYSQL_PASSWORD', '1234')
-#     HOST = os.getenv('MYSQL_HOST', 'db')  # Aquí cambiamos localhost por 'db', el nombre del servicio
-#     PORT = os.getenv('MYSQL_PORT', 3316)
-#     DATABASE = os.getenv('MYSQL_DATABASE', 'finca')
-
-#     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 import os
 
 class Config:
-    USER = os.getenv('MYSQL_USER', 'fincau')
-    PASSWORD = os.getenv('MYSQL_PASSWORD', 'fincac')
-    HOST = os.getenv('MYSQL_HOST', '85.239.241.150')  
-    PORT = os.getenv('MYSQL_PORT', 3306)
+    USER = os.getenv('DB_USER')
+    PASSWORD = os.getenv('DB_PASSWORD')
+    HOST = os.getenv('DB_HOST')  
+    PORT = os.getenv('DB_PORT')
     DATABASE = os.getenv('MYSQL_DATABASE', 'finca')
 
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
